5288b495aed7fc091df84b101d3b1ec13e442fcf
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-04-09 18:03:45 +0600

    38-6 Display form data in a table and form error handle

--- src/Components/ProductManagement/ProductForm.jsx
+++ src/Components/ProductManagement/ProductForm.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
-    
+const ProductForm = ({handleAddProduct}) => {
+    const [error, setError]= useState("")
     const handleProductSubmit = (e) => {
         e.preventDefault();
         const name = e.target.name.value;
         const price = e.target.price.value;
-        const quanity = e.target.quantity.value;
+        const quantity = e.target.quantity.value;
         // console.log(name, price, quanity);
+
+
+        if (name.length === 0) {
+            setError("Please Provide a product name");
+            return;
+        } else if (price.length === 0) {
+            setError("please provide a valid price");
+            return;
+        } else if (price < 0) {
+            setError("please provide a valid price");
+            return;
+        } else {
+            setError("");
+        }
 
         const newProduct = {
             name,
             price,
-            quanity
+            quantity
         }
-        console.log(newProduct);
+        // console.log(newProduct);
+        if (!error) {
+            handleAddProduct(newProduct);
+        }
     }
     return (
         <div>
@@ -27,7 +44,10 @@ const ProductForm = () => {
                 <input type="text" name='quantity' placeholder='product quanity' />
                 <br />
                 <input type="submit" value="submit" />
-            </form>     
+            </form>   
+            <p style={{color: "red"}}>
+                <small>{ error}</small>
+            </p>
         </div>
     );
 };
--- src/Components/ProductManagement/ProductManagement.jsx
+++ src/Components/ProductManagement/ProductManagement.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductTable from './ProductTable';
 import ProductForm from './ProductForm';
 
 const ProductManagement = () => {
-    
+
+    const [products, setProducts] = useState([])
+
+    const handleAddProduct = (newProduct) => {
+        const newProducts = [...products, newProduct];
+        setProducts(newProducts)
+    }
+
     return (
         <div>
-           <ProductForm></ProductForm>
-            <ProductTable></ProductTable>
+           <ProductForm handleAddProduct={handleAddProduct}></ProductForm>
+            <ProductTable products={products}></ProductTable>
         </div>
     );
 };
